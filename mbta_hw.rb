d571c983261f5c4e41bc0b7af9a0d9a812c7f571
Ben White <whtouche@gmail.com>
2014-06-11 07:35:59 -0400

    Add conditionals and math

--- mbta_hw.rb
+++ mbta_hw.rb
@@ -3,37 +3,49 @@
 # Destination line
 # Destination stop
 
-# This is a hash containing three arrays representing each line
 red_line = ['Alewife', 'Davis', 'Porter', 'Harvard', 'Central', 'Kendall/MIT', 'Park Street', 'South Station']
 green_line = ['Haymarket', 'Government Center', 'Park Street', 'Boylston', 'Arlington', 'Copley']
 orange_line = ['North Station', 'Haymarket', 'Park Street', 'State Street', 'Downtown Crossing', 'Chinatown', 'Tufts Medical Center']
-mbta = {
-  red: red_line,
-  green: green_line,
-  orange: orange_line
-}
-# puts mbta
-# puts mbta[:red][0] # prints 'Alewife'
 
-origin_line = mbta[:red]
-# puts origin_line
-origin_stop = mbta[:red][1]
-# origin_stop
-destination_line = mbta[:red]
-# puts destination_line
-destination_stop = mbta[:red][7]
-# puts destination_stop
+# This is a hash containing three arrays representing each line
+mbta = {red: red_line, orange: orange_line, green: green_line}
+
+park = 'Park Street'
+
+origin_line =
+
+origin_stop =
+
+destination_line =
+
+destination_stop =
+
+=begin
 
 if origin_line == destination_line
-  total_stops =
+  total_stops = (mbta[origin_line].index(origin_stop) - mbta[destination_line].index(destination_stop)).abs
+  puts "Total number of stops: #{first_line_stops}"
+end
+
+# move this above
+
+
+if origin_line != destination_line
+  first_line_stops = (mbta[origin_line].index(origin_stop) - mbta[origin_line].index(park)).abs
+  second_line_stops = (mbta[destination_line].index(park) - mbta[destination_line].index(destination_stop)).abs
+  total_stops = first_line_stops + second_line_stops
+  puts "Total number of stops: #{total_stops}"
+end
 
 # If line is the same
   # then one formula (arithmetic on indexes)
 # If line is different
   # then calculate origin to park PLUS park to destination
-# Need 3 park variables (park_red, park_green, park_orange)
+
 
 # same line or transfer?
 # subtract indexes for number of stops
 
 # .find_index('STRING') # Will find the index
+
+=end
