43196851dfe724c162da752d99b60ebf5d8d1939
Ben White <whtouche@gmail.com>
2014-06-10 20:55:49 -0400

    Create data structure
    *Hash of three arrays, one for each line

---
+++ mbta_hw.rb
@@ -0,0 +1,8 @@
+
+# This is a hash containing three arrays representing each line
+mbta = {
+  red: ['Alewife', 'Davis', 'Porter', 'Harvard', 'Central', 'Kendall/MIT', 'Park Street', 'South Station'],
+  green: ['Haymarket', 'Government Center', 'Park Street', 'Boylston', 'Arlington', 'Copley'],
+  orange: ['North Station', 'Haymarket', 'Park Street', 'State Street', 'Downtown Crossing', 'Chinatown', 'Tufts Medical Center']
+}
+puts mbta
