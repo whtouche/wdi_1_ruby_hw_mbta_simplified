aa04f3aacf29b362524e33cfb788e3dac1c65873
Ben White <whtouche@gmail.com>
2014-06-11 09:49:53 -0400

    Finish getting input from user
    *change data structure BACK (was not using variables created to hold arrays of each line, cleaner this way)

--- mbta_hw.rb
+++ mbta_hw.rb
@@ -1,16 +1,16 @@
-# Origin line
-# Origin stop
-# Destination line
-# Destination stop
 
-red_line = ['Alewife', 'Davis', 'Porter', 'Harvard', 'Central', 'Kendall/MIT', 'Park Street', 'South Station']
-green_line = ['Haymarket', 'Government Center', 'Park Street', 'Boylston', 'Arlington', 'Copley']
-orange_line = ['North Station', 'Haymarket', 'Park Street', 'State Street', 'Downtown Crossing', 'Chinatown', 'Tufts Medical Center']
+# red: Park Street = Park, Kendall/MIT = Kendall, South Station = Sstation
+# green: Government Center = Gcenter, Park Street = Park
+# orange: North Station = Nstation, Park Street = Park, State Street = State, Downtown Crossing = Downtown, Tufts Medical Center = Tufts
 
 # This is a hash containing three arrays representing each line
-mbta = {red: red_line, orange: orange_line, green: green_line}
+mbta = {
+  red: ['Alewife', 'Davis', 'Porter', 'Harvard', 'Central', 'Kendall', 'Park', 'Sstation'],
+  orange: ['Haymarket', 'Gcenter', 'Park', 'Boylston', 'Arlington', 'Copley'],
+  green: ['Nstation', 'Haymarket', 'Park', 'State', 'Downtown', 'Chinatown', 'Tufts']
+}
 
-park = 'Park Street'
+park = 'Park'
 
 puts "What line are you starting on?"
 origin_line = gets.chomp.downcase.to_sym
@@ -19,34 +19,40 @@ while !mbta.key?(origin_line)
   origin_line = gets.chomp.downcase.to_sym
 end
 
-origin_stop
+puts "What stop are you starting at?"
+origin_stop = gets.chomp.capitalize!
+while !mbta[origin_line].index(origin_stop)
+  puts "Could not find that stop. Please try again:"
+  origin_stop = gets.chomp.capitalize
+end
 
-destination_line
+puts "What line is your destination on?"
+destination_line = gets.chomp.downcase.to_sym
+while !mbta.key?(destination_line)
+  puts "Could not find that line. Please try again:"
+  destination_line = gets.chomp.downcase.to_sym
+end
 
-destination_stop
-
-
+puts "What is your destination stop?"
+destination_stop = gets.chomp.capitalize!
+while !mbta[destination_line].index(destination_stop)
+  puts "Could not find that stop. Please try again:"
+  destination_stop = gets.chomp.capitalize
+end
 
 if origin_line == destination_line
   total_stops = (mbta[origin_line].index(origin_stop) - mbta[destination_line].index(destination_stop)).abs
-  puts "Total number of stops: #{first_line_stops}"
-end
-
-
-if origin_line != destination_line
-  first_line_stops = (mbta[origin_line].index(origin_stop) - mbta[origin_line].index(park)).abs
-  second_line_stops = (mbta[destination_line].index(park) - mbta[destination_line].index(destination_stop)).abs
-  total_stops = first_line_stops + second_line_stops
   puts "Total number of stops: #{total_stops}"
 end
 
-# If line is the same
-  # then one formula (arithmetic on indexes)
-# If line is different
-  # then calculate origin to park PLUS park to destination
+if origin_line != destination_line
+# below: sum of stops on first line
+  first_line_stops = (mbta[origin_line].index(origin_stop) - mbta[origin_line].index(park)).abs
+# below: sum of stops on second line
+  second_line_stops = (mbta[destination_line].index(park) - mbta[destination_line].index(destination_stop)).abs
+##ABOVE: park = 2 - 1
 
-
-# same line or transfer?
-# subtract indexes for number of stops
-
-# .find_index('STRING') # Will find the index
+# below: sum of stops on first line + stops on second line
+  total_stops = first_line_stops + second_line_stops
+  puts "Total number of stops: #{total_stops}"
+end
